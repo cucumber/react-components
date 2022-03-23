efa0c1930425e224fcdb41d3daf833e938c8289e
John Lonergan <john.lonergan@gmail.com>
2022-03-23 10:36:05 +0000

    added missing tests for video attachment rendering video attachment summary is now the attachment name if provided otherwise it's the constant "Attached Video"

--- test/components/gherkin/AttachmentTest.tsx
+++ test/components/gherkin/AttachmentTest.tsx
@@ -21,19 +21,19 @@ describe('<Attachment>', () => {
     assert.strictEqual(video!.getAttribute('src'), 'data:video/mp4;base64,fake-base64')
   })
 
-  it('renders a video with a filename', () => {
+  it('renders a video with a name', () => {
     const binary = new Uint8Array(10)
     binary.fill(255, 0, binary.length)
     const attachment: messages.Attachment = {
       mediaType: 'video/mp4',
-      fileName: "theFileName",
+      fileName: "the attachment name",
       body: 'fake-base64',
       contentEncoding: messages.AttachmentContentEncoding.BASE64,
     }
     const { container } = render(<Attachment attachment={attachment} />)
     const summary = container.querySelector('details summary')
     const video = container.querySelector('video source')
-    assert.strictEqual(summary!.textContent, 'theFileName')
+    assert.strictEqual(summary!.textContent, 'the attachment name')
     assert.strictEqual(video!.getAttribute('src'), 'data:video/mp4;base64,fake-base64')
   })
 
@@ -52,19 +52,19 @@ describe('<Attachment>', () => {
     assert.strictEqual(img!.getAttribute('src'), 'data:image/png;base64,fake-base64')
   })
 
-  it('renders an image with a filename', () => {
+  it('renders an image with a name', () => {
     const binary = new Uint8Array(10)
     binary.fill(255, 0, binary.length)
     const attachment: messages.Attachment = {
       mediaType: 'image/png',
-      fileName: "theFileName",
+      fileName: "the attachment name",
       body: 'fake-base64',
       contentEncoding: messages.AttachmentContentEncoding.BASE64,
     }
     const { container } = render(<Attachment attachment={attachment} />)
     const summary = container.querySelector('details summary')
     const img = container.querySelector('img')
-    assert.strictEqual(summary!.textContent, 'theFileName')
+    assert.strictEqual(summary!.textContent, 'the attachment name')
     assert.strictEqual(img!.getAttribute('src'), 'data:image/png;base64,fake-base64')
   })
 
@@ -81,17 +81,17 @@ describe('<Attachment>', () => {
     assert.strictEqual(data!.textContent, 'hello')
   })
 
-  it('renders base64 encoded plaintext with a filename', () => {
+  it('renders base64 encoded plaintext with a name', () => {
     const attachment: messages.Attachment = {
       mediaType: 'text/plain',
-      fileName: "theFileName",
+      fileName: "the attachment name",
       body: Buffer.from('hello').toString('base64'),
       contentEncoding: messages.AttachmentContentEncoding.BASE64,
     }
     const { container } = render(<Attachment attachment={attachment} />)
     const summary = container.querySelector('details summary')
     const data = container.querySelector('details pre span')
-    assert.strictEqual(summary!.textContent, 'theFileName')
+    assert.strictEqual(summary!.textContent, 'the attachment name')
     assert.strictEqual(data!.textContent, 'hello')
   })
 
@@ -111,17 +111,17 @@ describe('<Attachment>', () => {
     )
   })
 
-  it('correctly renders ANSI characters with a filename', () => {
+  it('correctly renders ANSI characters with a name', () => {
     const attachment: messages.Attachment = {
       mediaType: 'text/x.cucumber.log+plain',
-      fileName: "theFileName",
+      fileName: "the attachment name",
       body: '\x1b[30mblack\x1b[37mwhite',
       contentEncoding: messages.AttachmentContentEncoding.IDENTITY,
     }
     const { container } = render(<Attachment attachment={attachment} />)
     const summary = container.querySelector('details summary')
     const data = container.querySelector('details > pre > span')
-    assert.strictEqual(summary!.textContent, 'theFileName')
+    assert.strictEqual(summary!.textContent, 'the attachment name')
     assert.strictEqual(
       data!.innerHTML,
       '<span style="color:#000">black<span style="color:#AAA">white</span></span>'
