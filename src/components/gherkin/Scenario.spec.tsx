cc68810b7c115d6359ef64099e435d0c15579de2
David Goss <david@davidgoss.co>
2022-04-24 22:27:29 +0100

    add some testing

---
+++ src/components/gherkin/Scenario.spec.tsx
@@ -0,0 +1,69 @@
+import { Envelope, GherkinDocument } from '@cucumber/messages'
+import { screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+
+import examplesTables from '../../../acceptance/examples-tables/examples-tables.feature'
+import { render } from '../../../test-utils'
+import UriContext from '../../UriContext'
+import { EnvelopesWrapper } from '../app/EnvelopesWrapper'
+import { Scenario } from './Scenario'
+
+describe('<Scenario/>', () => {
+  const getStatus = (element: HTMLElement) =>
+    within(element).getAllByRole('img', { hidden: true })[0].dataset.status
+
+  describe('with examples', () => {
+    const envelopes: Envelope[] = examplesTables as Envelope[]
+    const gherkinDocument: GherkinDocument = envelopes.find((env) => env.gherkinDocument)!
+      .gherkinDocument as GherkinDocument
+    const uri = gherkinDocument.uri!
+    const scenario = gherkinDocument.feature!.children[0].scenario!
+
+    it('should render the outline with worst result for each step', () => {
+      render(
+        <EnvelopesWrapper envelopes={envelopes}>
+          <UriContext.Provider value={uri}>
+            <Scenario scenario={scenario} />
+          </UriContext.Provider>
+        </EnvelopesWrapper>
+      )
+
+      expect(
+        screen.getByRole('heading', { name: 'Scenario Outline: eating cucumbers' })
+      ).toBeVisible()
+      expect(
+        screen.getByRole('heading', { name: 'Given there are <start> cucumbers' })
+      ).toBeVisible()
+      expect(screen.getByRole('heading', { name: 'When I eat <eat> cucumbers' })).toBeVisible()
+      expect(
+        screen.getByRole('heading', { name: 'Then I should have <left> cucumbers' })
+      ).toBeVisible()
+      const [step1, step2, step3] = screen.getAllByRole('listitem')
+      expect(getStatus(step1)).toEqual('PASSED')
+      expect(getStatus(step2)).toEqual('UNDEFINED')
+      expect(getStatus(step3)).toEqual('FAILED')
+    })
+
+    it('should render the results for individual examples - all passed', () => {
+      render(
+        <EnvelopesWrapper envelopes={envelopes}>
+          <UriContext.Provider value={uri}>
+            <Scenario scenario={scenario} />
+          </UriContext.Provider>
+        </EnvelopesWrapper>
+      )
+
+      userEvent.click(screen.getAllByRole('button', { name: 'Detail' })[0])
+
+      expect(screen.getByRole('heading', { name: 'Example: eating cucumbers' })).toBeVisible()
+      expect(screen.getByRole('heading', { name: 'Given there are 12 cucumbers' })).toBeVisible()
+      expect(screen.getByRole('heading', { name: 'When I eat 5 cucumbers' })).toBeVisible()
+      expect(screen.getByRole('heading', { name: 'Then I should have 7 cucumbers' })).toBeVisible()
+      const [step1, step2, step3] = screen.getAllByRole('listitem')
+      expect(getStatus(step1)).toEqual('PASSED')
+      expect(getStatus(step2)).toEqual('PASSED')
+      expect(getStatus(step3)).toEqual('PASSED')
+    })
+  })
+})
