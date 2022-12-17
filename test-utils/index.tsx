7105a40b404cfe8a8127aeb5ba94c7f50e82bba6
David Goss <david@davidgoss.co>
2022-12-17 16:32:54 +0000

    update types for react and react-dom (#328)
    * chore: update dependencies
    
    * fix: status icon typing
    
    * fix: update test wrapper

--- test-utils/index.tsx
+++ test-utils/index.tsx
@@ -29,7 +29,7 @@ const AllTheProviders: FunctionComponent<PropsWithChildren<{ options: TestRender
 }
 
 const customRender = (ui: ReactElement, options: Partial<TestRenderOptions> = {}) => {
-  const WrappedWithOptions: FunctionComponent = ({ children }) => (
+  const WrappedWithOptions: FunctionComponent<PropsWithChildren> = ({ children }) => (
     <AllTheProviders options={options}>{children}</AllTheProviders>
   )
   return render(ui, { wrapper: WrappedWithOptions })
