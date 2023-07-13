6dd4e0e5de1073ba83dc6ae8702373eed10187af
David Goss <david@davidgoss.co>
2023-07-13 15:50:32 +0100

    switch out mime-types for mime

--- src/components/gherkin/attachmentFilename.ts
+++ src/components/gherkin/attachmentFilename.ts
@@ -1,11 +1,11 @@
 import { Attachment } from '@cucumber/messages'
-import mimeTypes from 'mime-types'
+import mimeTypes from 'mime'
 
 export function attachmentFilename(attachment: Attachment): string {
   if (attachment.fileName) {
     return attachment.fileName
   }
-  const extension = mimeTypes.extension(attachment.mediaType)
+  const extension = mimeTypes.getExtension(attachment.mediaType)
   if (extension) {
     return `attachment.${extension}`
   }
