dcc60e11001614aab1428e08ed452a0686029f9f
David Goss <david@davidgoss.co>
2023-12-02 09:13:20 +0000

    some cleaning up

--- css-loader.mjs
+++ css-loader.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs'
 
 export async function load(url, context, nextLoad) {
   if (url.endsWith('.scss')) {
-    const content = fs.readFileSync(new URL(url), { encoding: "utf-8" });
+    const content = fs.readFileSync(new URL(url), { encoding: 'utf-8' });
     const classNamesMap = [...content.matchAll(/\.[\w\d]+ {/g)]
       .map(([fromCss]) => fromCss.substring(1, fromCss.length - 2))
       .reduce((prev, next) => ({
@@ -10,7 +10,7 @@ export async function load(url, context, nextLoad) {
         [next]: next
       }), {})
     return {
-      format: "json",
+      format: 'json',
       shortCircuit: true,
       source: JSON.stringify(classNamesMap)
     }
