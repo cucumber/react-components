90da9cc024bea5a5387591867dbac8a5d21caa20
David Goss <david@davidgoss.co>
2024-06-16 15:49:44 +0100

    feat: finalise json and text pattern

--- src/components/gherkin/attachment/Attachment.tsx
+++ src/components/gherkin/attachment/Attachment.tsx
@@ -1,4 +1,3 @@
-import * as messages from '@cucumber/messages'
 import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
@@ -10,9 +9,7 @@ import {
 } from '../../customise/index.js'
 import { ErrorMessage } from '../ErrorMessage.js'
 import defaultStyles from './Attachment.module.scss'
-import { base64Decode } from './base64Decode.js'
 import { Image } from './Image.js'
-import { Json } from './Json.js'
 import { Log } from './Log.js'
 import { Text } from './Text.js'
 import { Unknown } from './Unknown.js'
@@ -29,9 +26,9 @@ const DefaultRenderer: DefaultComponent<AttachmentProps, AttachmentClasses> = ({
   } else if (attachment.mediaType == 'text/x.cucumber.log+plain') {
     return <Log attachment={attachment} classes={styles} />
   } else if (attachment.mediaType.match(/^text\//)) {
-    return text(attachment, (s) => s, false, styles)
+    return <Text attachment={attachment} classes={styles} />
   } else if (attachment.mediaType.match(/^application\/json/)) {
-    return <Json attachment={attachment} classes={styles} />
+    return <Text attachment={attachment} prettify={prettyJSON} classes={styles} />
   } else {
     return <Unknown attachment={attachment} />
   }
@@ -47,27 +44,6 @@ export const Attachment: React.FunctionComponent<AttachmentProps> = (props) => {
     <ErrorBoundary fallback={<ErrorMessage message={`Attachment couldn't be rendered`} />}>
       <ResolvedRenderer {...props} />
     </ErrorBoundary>
-  )
-}
-
-function text(
-  attachment: messages.Attachment,
-  prettify: (body: string) => string,
-  dangerouslySetInnerHTML: boolean,
-  classes: AttachmentClasses
-) {
-  const body =
-    attachment.contentEncoding === 'IDENTITY' ? attachment.body : base64Decode(attachment.body)
-
-  const attachmentTitle = attachment.fileName ?? 'Attached Text (' + attachment.mediaType + ')'
-
-  return (
-    <Text
-      title={attachmentTitle}
-      content={prettify(body)}
-      html={dangerouslySetInnerHTML}
-      classes={classes}
-    />
   )
 }
 
--- src/components/gherkin/attachment/Text.tsx
+++ src/components/gherkin/attachment/Text.tsx
@@ -1,18 +1,22 @@
+import { Attachment } from '@cucumber/messages'
 import React, { FC } from 'react'
 
 import { AttachmentClasses } from '../../customise/index.js'
+import { useText } from './useText.js'
+
+const identity = (s: string) => s
 
 export const Text: FC<{
-  title: string
-  content: string
-  html: boolean
+  attachment: Attachment
+  prettify?: (body: string) => string
   classes: AttachmentClasses
-}> = ({ title, content, html, classes }) => {
+}> = ({ attachment, prettify = identity, classes }) => {
+  const { title, content } = useText(attachment)
   return (
     <details>
       <summary>{title}</summary>
       <pre className={classes.text}>
-        {html ? <span dangerouslySetInnerHTML={{ __html: content }} /> : <span>{content}</span>}
+        <span>{prettify(content)}</span>
       </pre>
     </details>
   )
