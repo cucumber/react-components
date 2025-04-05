8dbecdc189439232b5377dc2033b4464b6fb9d17
David Goss <david@davidgoss.co>
2025-04-05 10:51:05 +0100

    rename function for clarity

--- src/formatStatusRate.ts
+++ src/formatStatusRate.ts
@@ -1,4 +1,4 @@
-export function formatPassRate(passed: number, total: number) {
+export function formatStatusRate(passed: number, total: number) {
   return new Intl.NumberFormat(undefined, {
     style: 'percent',
   }).format(total > 0 ? passed / total : 0)
