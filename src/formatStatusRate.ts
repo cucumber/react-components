64c277dea0f22641e6e830afc79edea139da7c45
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2025-04-23 08:06:10 -0500

    #332 modifying rounding up behavior of passed percentage to rounding down and display of 1 decimal place

--- src/formatStatusRate.ts
+++ src/formatStatusRate.ts
@@ -1,5 +1,9 @@
 export function formatStatusRate(passed: number, total: number) {
+  const percentPassed = total > 0 ? passed / total : 0
+  const roundedDown = Math.floor(percentPassed * 1000) / 1000
+
   return new Intl.NumberFormat(undefined, {
     style: 'percent',
-  }).format(total > 0 ? passed / total : 0)
+    maximumFractionDigits: 2,
+  }).format(roundedDown)
 }
