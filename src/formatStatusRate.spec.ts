10ec6d41d21480cbdc3ad05c532361a9781accc6
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2025-04-24 09:55:44 -0500

    #332 adding support for one decimal place

--- src/formatStatusRate.spec.ts
+++ src/formatStatusRate.spec.ts
@@ -4,14 +4,14 @@ import { formatStatusRate } from './formatStatusRate.js'
 
 describe('formatStatusRate', () => {
   const examples: [passed: number, total: number, percentage: string][] = [
-    [13, 45, '28%'],
-    [5, 45, '11%'],
+    [13, 45, '28.8%'],
+    [5, 45, '11.1%'],
     [45, 45, '100%'],
     [0, 45, '0%'],
     [0, 0, '0%'],
-    [999, 1000, '99%'],
-    [99999, 100000, '99%'],
-    [9999999, 10000000, '99%'],
+    [999, 1000, '99.9%'],
+    [99999, 100000, '99.9%'],
+    [9999999, 10000000, '99.9%'],
   ]
 
   for (const [passed, total, percentage] of examples) {
--- src/formatStatusRate.ts
+++ src/formatStatusRate.ts
@@ -1,8 +1,9 @@
 export function formatStatusRate(passed: number, total: number) {
   const percentPassed = total > 0 ? passed / total : 0
-  const roundedDown = Math.floor(percentPassed * 100) / 100
+  const roundedDown = Math.floor(percentPassed * 1000) / 1000
 
   return new Intl.NumberFormat(undefined, {
     style: 'percent',
+    maximumFractionDigits: 1,
   }).format(roundedDown)
 }
