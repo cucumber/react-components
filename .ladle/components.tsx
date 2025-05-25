f17f1a78030d0071b655b63df137ad0ecc0fee02
David Goss <david@davidgoss.co>
2025-05-25 16:45:28 +0100

    Fix issues with Ladle build

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,1 +1,5 @@
-import './global.module.scss'
+import type { GlobalProvider } from '@ladle/react'
+import React from 'react'
+import styles from './components.module.scss'
+
+export const Provider: GlobalProvider = ({ children, globalState, storyMeta }) => <div className={styles.wrapper}>{children}</div>
