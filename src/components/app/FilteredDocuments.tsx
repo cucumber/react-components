ab4d150ab2fa95d2b15eac84532025982c12d091
David Goss <david@davidgoss.co>
2025-11-11 10:29:34 +0000

    make empty messaging consistent

--- src/components/app/FilteredDocuments.tsx
+++ src/components/app/FilteredDocuments.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
 
 import { useFilteredDocuments } from '../../hooks/useFilteredDocuments.js'
+import styles from './FilteredDocuments.module.scss'
 import { GherkinDocumentList } from './GherkinDocumentList.js'
-import { NoMatchResult } from './NoMatchResult.js'
 
 export const FilteredDocuments: FC = () => {
   const filtered = useFilteredDocuments()
@@ -10,7 +10,7 @@ export const FilteredDocuments: FC = () => {
   if (!filtered) {
     return null
   } else if (!filtered.length) {
-    return <NoMatchResult />
+    return <p className={styles.empty}>No scenarios were executed that match your query and/or filters.</p>
   }
   return <GherkinDocumentList gherkinDocuments={filtered} preExpand={true} />
 }
