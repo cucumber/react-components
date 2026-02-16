3ba553a0a5eedaf5dd334d36c24ec3cf4b2810ab
David Goss <david@davidgoss.co>
2026-02-16 09:03:35 +0000

    fix typing issues

--- src/components/app/rehypePlugins.ts
+++ src/components/app/rehypePlugins.ts
@@ -1,5 +1,5 @@
 import { defaultSchema } from 'hast-util-sanitize'
-import { PluggableList } from 'react-markdown'
+import { Options } from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import gfm from 'remark-gfm'
@@ -7,5 +7,5 @@ import gfm from 'remark-gfm'
 defaultSchema.tagNames?.push('mark', 'section', 'small')
 defaultSchema.attributes?.['*'].push('className')
 
-const plugins: PluggableList = [gfm, rehypeRaw, [rehypeSanitize, defaultSchema]]
+const plugins: Options['rehypePlugins'] = [gfm, rehypeRaw, [rehypeSanitize, defaultSchema]]
 export default plugins
