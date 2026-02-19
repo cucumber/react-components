cff2e0be319d66143e4f9446a7990a51a890b2cb
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2026-02-19 07:48:49 -0600

    Update expression to pass linter

--- src/components/gherkin/attachment/base64Decode.ts
+++ src/components/gherkin/attachment/base64Decode.ts
@@ -1,4 +1,4 @@
 export function base64Decode(body: string) {
   // Properly decode base64 to UTF-8 string
-  return new TextDecoder('utf-8').decode(Uint8Array.from(atob(body), c => c.charCodeAt(0)));
+  return new TextDecoder('utf-8').decode(Uint8Array.from(atob(body), (c) => c.charCodeAt(0)))
 }
