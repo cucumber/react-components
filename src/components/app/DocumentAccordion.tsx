8456894079689b0699bae6b9d0e021902fb13694
David Goss <david@davidgoss.co>
2026-03-15 07:37:57 +0000

    Reimplement accordion with react-aria

--- src/components/app/DocumentAccordion.tsx
+++ src/components/app/DocumentAccordion.tsx
@@ -3,27 +3,18 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import type { FC, ReactNode } from 'react'
 import {
-  Accordion,
-  AccordionItem,
-  AccordionItemButton,
-  AccordionItemHeading,
-  AccordionItemPanel,
-} from 'react-accessible-accordion'
-import { v4 as uuidv4 } from 'uuid'
+  Button,
+  Disclosure,
+  DisclosureGroup,
+  DisclosurePanel,
+  Heading,
+} from 'react-aria-components'
 
 import { StatusIcon } from '../gherkin/index.js'
 import styles from './DocumentAccordion.module.scss'
 
-const idByUri = new Map<string, string>()
-function getIdByUri(uri: string): string {
-  if (!idByUri.has(uri)) {
-    idByUri.set(uri, uuidv4())
-  }
-  return idByUri.get(uri) as string
-}
-
 interface DocumentAccordionProps {
-  expanded: ReadonlyArray<string>
+  expanded: Iterable<string>
   children: ReactNode
 }
 
@@ -34,16 +25,14 @@ interface DocumentAccordionItemProps {
 }
 
 export const DocumentAccordion: FC<DocumentAccordionProps> = ({ expanded, children }) => {
-  const preExpand = expanded.map((uri) => getIdByUri(uri))
   return (
-    <Accordion
-      allowMultipleExpanded={true}
-      allowZeroExpanded={true}
-      preExpanded={preExpand}
+    <DisclosureGroup
+      allowsMultipleExpanded
+      defaultExpandedKeys={expanded}
       className={styles.accordion}
     >
       {children}
-    </Accordion>
+    </DisclosureGroup>
   )
 }
 
@@ -52,23 +41,20 @@ export const DocumentAccordionItem: FC<DocumentAccordionItemProps> = ({
   uri,
   children,
 }) => {
-  const id = getIdByUri(uri)
   return (
-    <AccordionItem uuid={id} className={styles.accordionItem}>
-      <AccordionItemHeading>
-        <AccordionItemButton className={styles.accordionButton}>
-          <FontAwesomeIcon
-            className={styles.accordionChevron}
-            aria-hidden="true"
-            icon={faChevronRight}
-          />
+    <Disclosure id={uri} className={styles.item}>
+      <Heading className={styles.heading}>
+        <Button slot="trigger" className={styles.button}>
+          <FontAwesomeIcon className={styles.chevron} aria-hidden="true" icon={faChevronRight} />
           <span className={styles.icon}>
             <StatusIcon status={status} />
           </span>
           <span>{uri}</span>
-        </AccordionItemButton>
-      </AccordionItemHeading>
-      <AccordionItemPanel className={styles.accordionPanel}>{children}</AccordionItemPanel>
-    </AccordionItem>
+        </Button>
+      </Heading>
+      <DisclosurePanel>
+        <div className={styles.content}>{children}</div>
+      </DisclosurePanel>
+    </Disclosure>
   )
 }
