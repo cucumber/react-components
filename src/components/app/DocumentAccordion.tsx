ad44ebd86e8cf2b98e9b44f4bc9811e044fecca3
David Goss <david@davidgoss.co>
2026-03-15 15:49:31 +0000

    Render accordion contents lazily (#495)

--- src/components/app/DocumentAccordion.tsx
+++ src/components/app/DocumentAccordion.tsx
@@ -1,29 +1,21 @@
 import type { TestStepResultStatus } from '@cucumber/messages'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import type { FC, ReactNode } from 'react'
+import { type FC, type ReactElement, type ReactNode, useContext, useState } from 'react'
 import {
-  Accordion,
-  AccordionItem,
-  AccordionItemButton,
-  AccordionItemHeading,
-  AccordionItemPanel,
-} from 'react-accessible-accordion'
-import { v4 as uuidv4 } from 'uuid'
+  Button,
+  Disclosure,
+  DisclosureGroup,
+  DisclosurePanel,
+  DisclosureStateContext,
+  Heading,
+} from 'react-aria-components'
 
 import { StatusIcon } from '../gherkin/index.js'
 import styles from './DocumentAccordion.module.scss'
 
-const idByUri = new Map<string, string>()
-function getIdByUri(uri: string): string {
-  if (!idByUri.has(uri)) {
-    idByUri.set(uri, uuidv4())
-  }
-  return idByUri.get(uri) as string
-}
-
 interface DocumentAccordionProps {
-  expanded: ReadonlyArray<string>
+  expanded: Iterable<string>
   children: ReactNode
 }
 
@@ -33,17 +25,26 @@ interface DocumentAccordionItemProps {
   children: ReactNode
 }
 
+const LazyContent: FC<{ children: ReactElement }> = ({ children }) => {
+  const state = useContext(DisclosureStateContext)
+  const [hasBeenExpanded, setHasBeenExpanded] = useState(state?.isExpanded ?? false)
+
+  if (state?.isExpanded && !hasBeenExpanded) {
+    setHasBeenExpanded(true)
+  }
+
+  return hasBeenExpanded ? children : null
+}
+
 export const DocumentAccordion: FC<DocumentAccordionProps> = ({ expanded, children }) => {
-  const preExpand = expanded.map((uri) => getIdByUri(uri))
   return (
-    <Accordion
-      allowMultipleExpanded={true}
-      allowZeroExpanded={true}
-      preExpanded={preExpand}
+    <DisclosureGroup
+      allowsMultipleExpanded
+      defaultExpandedKeys={expanded}
       className={styles.accordion}
     >
       {children}
-    </Accordion>
+    </DisclosureGroup>
   )
 }
 
@@ -52,23 +53,22 @@ export const DocumentAccordionItem: FC<DocumentAccordionItemProps> = ({
   uri,
   children,
 }) => {
-  const id = getIdByUri(uri)
   return (
-    <AccordionItem uuid={id} className={styles.accordionItem}>
-      <AccordionItemHeading>
-        <AccordionItemButton className={styles.accordionButton}>
-          <FontAwesomeIcon
-            className={styles.accordionChevron}
-            aria-hidden="true"
-            icon={faChevronRight}
-          />
+    <Disclosure id={uri} className={styles.item}>
+      <Heading className={styles.heading}>
+        <Button slot="trigger" className={styles.button}>
+          <FontAwesomeIcon className={styles.chevron} aria-hidden="true" icon={faChevronRight} />
           <span className={styles.icon}>
             <StatusIcon status={status} />
           </span>
           <span>{uri}</span>
-        </AccordionItemButton>
-      </AccordionItemHeading>
-      <AccordionItemPanel className={styles.accordionPanel}>{children}</AccordionItemPanel>
-    </AccordionItem>
+        </Button>
+      </Heading>
+      <DisclosurePanel>
+        <LazyContent>
+          <div className={styles.content}>{children}</div>
+        </LazyContent>
+      </DisclosurePanel>
+    </Disclosure>
   )
 }
