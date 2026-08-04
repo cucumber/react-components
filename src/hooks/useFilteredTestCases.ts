8cb7d23ff16e964408bf03b94e4e2bfbda9c2c5f
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-08-04 23:04:57 +0500

    Resolved Merge Conflicts

--- src/hooks/useFilteredTestCases.ts
+++ src/hooks/useFilteredTestCases.ts
@@ -27,7 +27,6 @@ export function useFilteredTestCases(): {
       }),
     [allTestCasesFinished, cucumberQuery, hideStatuses, tagExpression]
   )
-  console.log(candidates[0])
   const searchResult = useSearchResult()
   const [results, setResults] = useState<ReadonlyArray<ExpandedTestCase<TestCaseFinished>>>()
 
